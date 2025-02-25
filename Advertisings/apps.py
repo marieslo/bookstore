0b530f3cfb9e73b1afb06d941f29d5cff4c446b5
marieslo <maria.slovohotova@gmail.com>
2025-02-25 18:05:47 +0400

    structure has refactored

--- Advertisings/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdvertisementConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "advertisement"
-
-    def ready(self):
-        from .signals import notify_user_reply
--- Advertisings/filters.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-from .models import OneTimeCode
-
-
-class OneTimeCodeFilter(django_filters.Filter):
-    class Meta:
-        model = OneTimeCode
-        fields = ['code']
